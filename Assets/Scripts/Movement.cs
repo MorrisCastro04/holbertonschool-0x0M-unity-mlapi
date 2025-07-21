2b26610ea97c20dbcc5b198e06b697d2302ea227
Morris Jomar <jomarcastro2004@gmail.com>
2025-07-21 18:10:12 -0400

    update server to see other player movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,25 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 
 public class Movement : NetworkBehaviour
 {
-    public float currentSpeed = 0f;
     public float maxSpeed = 10f;
-    public float currentRotation = 0f;
     public float maxRotation = 100f;
 
-    // Update is called once per frame
-    void Update()
+    public NetworkVariable<Vector3> position = new NetworkVariable<Vector3>();
+    public NetworkVariable<Quaternion> rotation = new NetworkVariable<Quaternion>();
+
+    private void Update()
     {
         if (IsLocalPlayer)
         {
-            currentSpeed = maxSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
-            currentRotation = maxRotation * Input.GetAxis("Horizontal") * Time.deltaTime;
 
-            transform.Translate(0, 0, currentSpeed);
-            transform.Rotate(0, currentRotation, 0);
+            float move = maxSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+            float turn = maxRotation * Input.GetAxis("Horizontal") * Time.deltaTime;
+
+            transform.Translate(0, 0, move);
+            transform.Rotate(0, turn, 0);
+
+            UpdatePlayerPositionServerRpc(transform.position.x, transform.position.y, transform.position.z);
+            UpdatePlayerRotationServerRpc(transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w);
         }
+        else
+        {
+            transform.position = position.Value;
+            transform.rotation = rotation.Value;
+        }
+    }
+
+    [ServerRpc]
+    void UpdatePlayerPositionServerRpc(float x, float y, float z)
+    {
+        position.Value = new Vector3(x, y, z);
+    }
+
+    [ServerRpc]
+    void UpdatePlayerRotationServerRpc(float x, float y, float z, float w)
+    {
+        rotation.Value = new Quaternion(x, y, z, w);
     }
 }
